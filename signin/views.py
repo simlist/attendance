f908504a5fce8a5c338c68c19fde15e70706b89b
Meir S. List <simlist@gmail.com>
2018-11-04 17:08:08 -0500

    Made index handler grade list.

--- signin/views.py
+++ signin/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework import generics
+from rest_framework import mixins
+
+from . import models
+from . import serializers
+
 # Create your views here.
-def index(request):
-    pass
+def index(generics.ListCreateAPIView):
+    query_set = Grade.objects.all()
+    serializer_class = serializers.GradeSerializer
