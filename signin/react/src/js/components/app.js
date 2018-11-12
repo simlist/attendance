49040edc268947911a6c00a26847b2d58ba2b9e7
simlist <simlist@gmail.com>
2018-11-11 21:24:42 -0500

    Added react app.

---
+++ signin/react/src/js/components/app.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+        <div className="classes-container">
+          <a className="class">2Y</a>
+          <a className="class">3Y</a>
+          <a className="class">Pre-K</a>
+          <a className="class">First Grade</a>
+          <a className="class">Second Grade</a>
+          <a className="class">Third Grade</a>
+          <a className="class">Fourth Grade</a>
+          <a className="class">Fifth Grade</a>
+          <a className="class">Sixth Grade</a>
+          <a className="class">Seventh Grade</a>
+          <a className="class">Eight Grade</a>
+        </div>
+    );}
+}
+export default App;
